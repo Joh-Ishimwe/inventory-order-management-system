053eb4632e1586436e5a799450caab75cb93b277
Joh-Ishimwe <j.ishimwe3@alustudent.com>
2026-08-07 02:45:19 +0200

    build: add run_all.sql to build the whole database in one script

---
+++ sql/run_all.sql
@@ -0,0 +1,15 @@
+-- Master build script. Run from the project root:
+--   mysql -u root -p < sql/run_all.sql
+-- or SOURCE sql/run_all.sql from an open client session.
+
+SOURCE sql/schema/00_drop_all.sql;
+SOURCE sql/schema/02_tables.sql;
+SOURCE sql/schema/03_business_rules.sql;
+SOURCE sql/procedures/procedures.sql;
+SOURCE sql/triggers/triggers.sql;
+SOURCE sql/views/views.sql;
+SOURCE sql/indexes/01_indexes.sql;
+SOURCE sql/seed/01_sample_data.sql;
+
+-- Tests aren't included: they call real procedures and leave rows behind.
+-- Run separately: tests/00_test_helpers.sql, then 01, 02, 03 in order.
